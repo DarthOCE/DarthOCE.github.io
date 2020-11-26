d17abf60e67a277a53a874f99ee7437c93a9deb8
DarthOCE <liamdagamer8667@gmail.com>
2020-11-26 16:24:55 +1100

    Adding tweaks that i like

---
+++ AmongLock-master/README.md
@@ -0,0 +1,9 @@
+# AmongLock
+You were not the owner of this device
+
+## Installation
+1. Add this repository to your Package Manager: https://repo.litten.love
+2. Install AmongLock
+
+## Compiling
+No dependencies, just make package (make clean package)
---
+++ AmongLock-master/Tweak/AmongLock.h
@@ -0,0 +1,134 @@
+#import <UIKit/UIKit.h>
+#import <AVFoundation/AVFoundation.h>
+#import <AudioToolbox/AudioServices.h>
+#import <sys/utsname.h>
+#import <Cephei/HBPreferences.h>
+
+HBPreferences* preferences;
+
+extern BOOL enabled;
+
+BOOL isBlocked = NO;
+int unlockSource;
+BOOL isiPhone = NO;
+BOOL isiPod = NO;
+BOOL isiPad = NO;
+
+AVQueuePlayer* backgroundPlayer;
+AVPlayerLooper* backgroundPlayerLooper;
+AVPlayerItem* backgroundPlayerItem;
+AVPlayerLayer* backgroundPlayerLayer;
+
+AVPlayer* ejectionPlayer;
+AVPlayerItem* ejectionPlayerItem;
+AVPlayerLayer* ejectionPlayerLayer;
+
+AVPlayer* emergencyCallPlayer;
+AVPlayerItem* emergencyCallPlayerItem;
+AVPlayerLayer* emergencyCallPlayerLayer;
+
+UIView* viewToBlockPasscode;
+
+UIImageView* passcodeBackground;
+UIImageView* passcodeButton;
+
+UIImageView* emergencyButtonImage;
+UIImageView* backspaceButtonImage;
+UIImageView* cancelButtonImage;
+
+// background video
+BOOL enableBackgroundVideoSwitch = YES;
+BOOL useAsWallpaperSwitch = NO;
+
+// ejection video
+BOOL enableEjectionVideoSwitch = YES;
+
+// emergency call video
+BOOL enableEmergencyCallVideoSwitch = YES;
+
+// bulbs
+BOOL enableBulbsSwitch = YES;
+
+// passcode
+BOOL themePasscodeSwitch = YES;
+BOOL wrongPasscodeAnimationSwitch = YES;
+
+// audio
+BOOL passcodeAppearSoundSwitch = YES;
+BOOL passcodeDisappearSoundSwitch = YES;
+BOOL wrongPasscodeSoundSwitch = YES;
+BOOL passcodeButtonSoundSwitch = YES;
+
+// hiding
+BOOL hideEmergencyButtonSwitch = NO;
+BOOL invisibleEmergencyButtonSwitch = NO;
+BOOL hideCancelButtonSwitch = NO;
+BOOL invisibleCancelButtonSwitch = NO;
+BOOL hideFaceIDAnimationSwitch = YES;
+
+// miscellaneous
+BOOL tapToDismissEjectionSwitch = YES;
+BOOL changeFrameWhenRotatingSwitch = YES;
+
+@interface CSPasscodeViewController : UIViewController
+- (void)ejectionVideoFinishedPlaying;
+- (void)layoutPlayerLayer:(NSNotification *)notification;
+@end
+
+@interface CSCoverSheetViewController : UIViewController
+- (void)layoutPlayerLayer:(NSNotification *)notification;
+@end
+
+@interface MTMaterialView : UIView
+@end
+
+@interface SBUISimpleFixedDigitPasscodeEntryField : UIView
+@end
+
+@interface SBUINumericPasscodeEntryFieldBase : UIView
+@property(assign, nonatomic)unsigned long long maxNumbersAllowed;
+@end
+
+@interface SBUIPasscodeTextField : UIView
+@property(assign, nonatomic)id delegate;
+- (void)setText:(NSString *)arg1;
+@end
+
+@interface SBNumberPadWithDelegate : UIControl
+@end
+
+@interface SBPasscodeNumberPadButton : UIControl
+- (void)changePasscodeButtonImages;
+- (void)failedPasscodeAttemptAnimation:(NSNotification *)notification;
+@end
+
+@interface TPNumberPadButton : UIControl
+@end
+
+@interface SBLockScreenManager : NSObject
++ (id)sharedInstance;
+- (BOOL)isUILocked;
+@end
+
+@interface SBUIProudLockIconView : UIView
+- (void)receiveHideNotification:(NSNotification *)notification;
+@end
+
+@interface SBFLockScreenDateView : UIView
+- (void)receiveHideNotification:(NSNotification *)notification;
+@end
+
+@interface NCNotificationListView : UIView
+- (void)receiveHideNotification:(NSNotification *)notification;
+@end
+
+@interface CSQuickActionsButton : UIView
+- (void)receiveHideNotification:(NSNotification *)notification;
+@end
+
+@interface CSTeachableMomentsContainerView : UIView
+- (void)receiveHideNotification:(NSNotification *)notification;
+@end
+
+@interface SBUIButton : UIButton
+@end
